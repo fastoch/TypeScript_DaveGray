a4b3b003f8210176f1d4534a68fdd80c4e254895
fastoch <fastoch@ik.me>
2024-12-08 17:48:23 +0100

    @135 first Class, visibility modifiers and first instantiation

--- LESSON06/build/js/lesson06.js
+++ LESSON06/build/js/lesson06.js
@@ -3,7 +3,22 @@
 // inside of the class, properties and methods are called "members"
 // a class needs a constructor (or more) in order to be instantiated (to create objects from it)
 class Coder {
-    constructor(name) {
+    constructor(name, music, age, lang) {
         this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
     }
 }
+// Visibility modifiers:
+// - public = visible from anywhere
+// - readonly = cannot be changed once assigned
+// - private = can only be accessed in the class
+// - protected = between public and private
+//    > subclasses can access protected members of their superclass
+// Now let's instantiate our class
+const Dave = new Coder('Dave', 'Doom OST', 42, 'english');
--- LESSON06/src/lesson06.ts
+++ LESSON06/src/lesson06.ts
@@ -5,13 +5,27 @@
 // a class needs a constructor (or more) in order to be instantiated (to create objects from it)
 
 class Coder {
-  name: string 
-  music: string
-  age: number
-  lang: string
-
-  constructor(name: string) {
+  
+  constructor(
+    public readonly name: string, 
+    public music: string, 
+    private age: number, 
+    protected lang: string
+  ) {
     this.name = name
+    this.music = music
+    this.age = age
+    this.lang = lang
   }
 }
 
+// Visibility modifiers:
+// - public = visible from anywhere
+// - readonly = cannot be changed once assigned
+// - private = can only be accessed in the class
+// - protected = between public and private
+//    > subclasses can access protected members of their superclass
+
+
+// Now let's instantiate our class
+const Dave = new Coder('Dave', 'Doom OST', 42, 'english')
