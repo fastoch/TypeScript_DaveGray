9be4c6718f32e38618c9535373a1baa5b9f8e5f5
fastoch <fastoch@ik.me>
2024-12-08 11:48:06 +0100

    @130 lesson 06 - Classes

--- LESSON06/build/js/lesson06.js
+++ LESSON06/build/js/lesson06.js
@@ -1,1 +1,9 @@
 "use strict";
+// --------------- CLASSES --------------------
+// inside of the class, properties and methods are called "members"
+// a class needs a constructor (or more) in order to be instantiated (to create objects from it)
+class Coder {
+    constructor(name) {
+        this.name = name;
+    }
+}
---
+++ LESSON06/src/lesson06.ts
@@ -0,0 +1,17 @@
+// --------------- CLASSES --------------------
+
+// inside of the class, properties and methods are called "members"
+
+// a class needs a constructor (or more) in order to be instantiated (to create objects from it)
+
+class Coder {
+  name: string 
+  music: string
+  age: number
+  lang: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
