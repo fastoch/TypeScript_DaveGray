228cca9bc737b1f5226807aa072934bffaa1de82
fastoch <fastoch@ik.me>
2024-12-15 21:56:52 +0100

    @160 index signatures

--- LESSON07/src/lesson07.ts
+++ LESSON07/src/lesson07.ts
@@ -1,3 +1,10 @@
 //--------------- Index signatures & keyof assertions -------------------
 
+// Index signatures are useful when you create an object but don't know the exact names of the object keys.
+// But you do know the shape of the object and can declare the type of the keys and values.
 
+// The other reason for using index signatures is because TS requires them if you attempt to access an object's property dynamically.
+
+interface transactionObj {
+  
+}
