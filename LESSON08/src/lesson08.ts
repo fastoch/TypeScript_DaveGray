971d27b31e070fb5d3e2d3e7e41bf437c81c68bb
fastoch <fastoch@ik.me>
2024-12-20 15:23:07 +0100

    @183 starting chap 8 - Generics

--- LESSON08/src/lesson08.ts
+++ LESSON08/src/lesson08.ts
@@ -1,4 +1,10 @@
-// ------------------------Chapter 8: GENERICS----------------------------------------------------
+// ----------------------------Chapter 8: GENERICS-----------------------------------------------
 
-
+/*
+The whole idea behind TypeScript is to define strict types for a type-safe developer experience.
+To achieve that, we're usually as detailed as possible.
+However, TS does allow for GENERICS, because sometimes we simply don't know what types will be 
+passed into a function/interface/type alias/class...
+Generics allow us to provide a placeholder, a type "variable" if you will.
+*/
 
