93940303932a051f1ae850ce16405a406c696001
fastoch <fastoch@ik.me>
2024-12-23 19:00:17 +0100

    @210 Utility Type 1 = Partial

--- build/js/lesson09.js
+++ build/js/lesson09.js
@@ -1,5 +1,14 @@
 "use strict";
 // ----------UTILITY TYPES ----------------
-/*
-
-*/ 
+// We create a function to update some of the properties of a given Assignment object
+const updateAssignment = (assign, propsToUpdate) => {
+    return Object.assign(Object.assign({}, assign), propsToUpdate); // overwrite the properties with provided values
+};
+// le'ts create an assignment object
+const assign1 = {
+    studentId: '001',
+    title: "Final Project",
+    grade: 0
+};
+console.log(assign1);
+console.log(updateAssignment(assign1, { grade: 95 }));
--- src/lesson09.ts
+++ src/lesson09.ts
@@ -1,5 +1,27 @@
 // ----------UTILITY TYPES ----------------
 
-/*
+// TS offers many utility types that are heplful for common type transformations.
 
-*/
+// 1. Partial
+
+interface Assignment {
+  studentId: string,
+  title: string,
+  grade: number,
+  verified?: boolean
+}
+
+// We create a function to update some of the properties of a given Assignment object
+const updateAssignment = (assign: Assignment, propsToUpdate: Partial<Assignment>): Assignment => {
+  return {...assign, ...propsToUpdate } // overwrite the properties with provided values
+}
+
+// le'ts create an assignment object
+const assign1: Assignment = {
+  studentId: '001',
+  title: "Final Project",
+  grade: 0
+}
+
+console.log(assign1) 
+console.log(updateAssignment(assign1, { grade: 95 })) 
