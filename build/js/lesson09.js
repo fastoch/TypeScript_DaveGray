c3e43e9af7cc695c05972944becbd5d187463763
fastoch <fastoch@ik.me>
2024-12-24 12:51:53 +0100

    @218 Utility Types - Pick & Omit

--- build/js/lesson09.js
+++ build/js/lesson09.js
@@ -6,9 +6,44 @@ const updateAssignment = (assign, propsToUpdate) => {
 };
 // le'ts create an assignment object
 const assign1 = {
-    studentId: '001',
+    studentId: 'f001',
     title: "Final Project",
     grade: 0
 };
+// let's log the assignment to the console before and after updating it
 console.log(assign1);
-console.log(updateAssignment(assign1, { grade: 95 }));
+const assignGraded = updateAssignment(assign1, { grade: 95 });
+console.log(assignGraded);
+// 2. Required & Readonly
+// In this function, all properties are required, including the optional one (verified)
+const recordAssignment = (assign) => {
+    // some logi to send the assignment to a database 
+    return assign;
+};
+// now let's create a verified assignment that will be readonly 
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+// Readonly properties cannot be overwritten
+// We need to add a value to the 'verified' property, otherwise we couldn't record assignVerified.
+// Likewise, we couldn't record assignGraded. but we could do the following:
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
+// 3. Record (a very popular utility type)
+// in the following object, keys (or properties) will be strings and values will be strings
+const hexColorMap = {
+    red: "FF0000",
+    green: "00FF00",
+    blue: "0000FF"
+};
+const finalGrades = {
+    Sarah: "A",
+    Kelly: "U"
+};
+const gradeData = {
+    Sarah: { assign1: 16, assign2: 14 },
+    Kelly: { assign1: 8, assign2: 10 }
+};
+// we have picked what we want to use from our Assignment interface
+const score = {
+    studentId: "p002",
+    grade: 82
+};
+// Omit will do the opposite
--- src/lesson09.ts
+++ src/lesson09.ts
@@ -18,10 +18,72 @@ const updateAssignment = (assign: Assignment, propsToUpdate: Partial<Assignment>
 
 // le'ts create an assignment object
 const assign1: Assignment = {
-  studentId: '001',
+  studentId: 'f001',
   title: "Final Project",
   grade: 0
 }
 
+// let's log the assignment to the console before and after updating it
 console.log(assign1) 
-console.log(updateAssignment(assign1, { grade: 95 })) 
+const assignGraded = updateAssignment(assign1, { grade: 95 })
+console.log(assignGraded) 
+
+
+// 2. Required & Readonly
+
+// In this function, all properties are required, including the optional one (verified)
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+  // some logi to send the assignment to a database 
+  return assign
+}
+
+// now let's create a verified assignment that will be readonly 
+const assignVerified: Readonly<Assignment> = { ...assignGraded, verified: true }
+// Readonly properties cannot be overwritten
+
+// We need to add a value to the 'verified' property, otherwise we couldn't record assignVerified.
+// Likewise, we couldn't record assignGraded. but we could do the following:
+recordAssignment({ ...assignGraded, verified: true })
+
+
+// 3. Record (a very popular utility type)
+
+// in the following object, keys (or properties) will be strings and values will be strings
+const hexColorMap: Record<string, string> = {
+  red: "FF0000",
+  green: "00FF00",
+  blue: "0000FF"
+}
+
+// But we can also create string literal types
+type Students = "Sarah" | "Kelly"
+type LetterGrades = "A" | "B" | "C" | "D" | "U"
+
+const finalGrades: Record<Students, LetterGrades> = {
+  Sarah: "A",
+  Kelly: "U"
+}
+
+// we can also do that with an interface
+interface Grades {
+  assign1: number,
+  assign2: number
+}
+
+const gradeData: Record<Students, Grades> = {
+  Sarah: { assign1: 16, assign2: 14 },
+  Kelly: { assign1: 8, assign2: 10 }
+}
+
+
+// 4. Pick & Omit
+
+type AssignResult = Pick<Assignment, "studentId" | "grade">
+// we have picked what we want to use from our Assignment interface
+
+const score: AssignResult = {
+  studentId: "p002",
+  grade: 82
+}
+
+// Omit will do the opposite
