c2559e7ac536ad1c1edc1896379c81aa6ba051ed
fastoch <fastoch@ik.me>
2024-12-28 14:51:33 +0100

    @258 implementing class FullList methods

--- Vite/vite-ts/src/model/FullList.ts
+++ Vite/vite-ts/src/model/FullList.ts
@@ -11,31 +11,36 @@ interface List {
 
 export default class FullList implements List {
 
-  // we'll make the constructor private because we want to create a singleton
-  // that means there will only be one instance of this class created
+  // creating the only instance of our class (a singleton)
+  static instance: FullList = new FullList()
+  // because we'll only have one list in our application
+  
+  // we'll make the constructor private so we cannot modify this unique instance
   private constructor(private _list: ListItem[] = []) {}
 
   get list(): ListItem[] {
     return this._list
   }
 
-  load(): void {
-
-  }
-
   save(): void {
-
+    localStorage.setItem("myList", JSON.stringify(this._list))
   }
 
   clearList(): void {
-    
+    this._list = []
+    this.save()
   }
 
   addItem(itemObj: ListItem): void {
-
+    this._list.push(itemObj)
+    this.save()
   }
 
   removeItem(id: string): void {
     
+  }
+
+  load(): void {
+
   }
 }
