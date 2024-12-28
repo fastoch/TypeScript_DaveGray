abd819876e228a9c4f796adc77cee296ed5921a9
fastoch <fastoch@ik.me>
2024-12-28 11:33:20 +0100

    @252 implementing the ListItem class

--- Vite/vite-ts/src/main.ts
+++ Vite/vite-ts/src/main.ts
@@ -1,2 +1,6 @@
 import './css/style.css'
 
+/* We will start by creating a data model for the list items
+   That will be inside of the src directory => 'model' directory
+   Inside of this model directory, we'll create a new file = ListItem.ts
+*/
---
+++ Vite/vite-ts/src/model/ListItem.ts
@@ -0,0 +1,43 @@
+export interface Item {
+  id: string,
+  item: string,
+  checked: boolean
+}
+
+// default export from this file
+export default class ListItem implements Item {
+
+  constructor(
+    private _id: string = '',  // the underscore is there to indicate it's a private property
+    private _item: string = '',
+    private _checked: boolean = false
+  ) {} 
+  // no need to have any assignment between the curly braces because already done when passing
+  // the parameters to the constructor
+
+  // we still need to implement our interface with getters & setters
+  get id(): string {
+    return this._id
+  }
+
+  get item(): string {
+    return this._item
+  }
+
+  get checked(): boolean {
+    return this._checked
+  }
+
+  set id(id: string) {
+    this._id = id
+  }
+
+  set item(item: string) {
+    this._item = item
+  }
+
+  set checked(checked: boolean) {
+    this._checked = checked
+  }
+}
+
