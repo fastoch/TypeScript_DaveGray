16fde111c2bb620eb735d01e548572d1d0f63f38
fastoch <fastoch@ik.me>
2024-12-30 17:01:42 +0100

    @280 implementing main.ts (chap 11)

--- Vite/vite-ts/src/main.ts
+++ Vite/vite-ts/src/main.ts
@@ -6,5 +6,18 @@ import ListItem from './model/ListItem'
 import ListTemplate from './template/ListTemplate'
 
 const initApp = (): void => {
-  
+  const fullList = FullList.instance
+  const template = ListTemplate.instance
+
+  const itemEntryForm = document.getElementById("itemEntryForm") as HTMLFormElement //index.html > line 19
+  itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+    event.preventDefault() // so the form submission doesn't reload the page
+    // ...
+  })
+
+  const clearItems = document.getElementById("clearItemsButton") as HTMLButtonElement
+  clearItems.addEventListener('click', )
 }
+
+// once the DOM content is loaded, run our initApp function
+document.addEventListener("DOMContentLoaded", initApp)
