350c2d6ccc52c64aae8d7adaef9c38d6e2eef479
fastoch <fastoch@ik.me>
2025-01-01 00:39:01 +0100

    @283 handling the new item form

--- Vite/vite-ts/src/main.ts
+++ Vite/vite-ts/src/main.ts
@@ -9,14 +9,25 @@ const initApp = (): void => {
   const fullList = FullList.instance
   const template = ListTemplate.instance
 
+  // handle the form that allows us to add new list items
   const itemEntryForm = document.getElementById("itemEntryForm") as HTMLFormElement //index.html > line 19
   itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
     event.preventDefault() // so the form submission doesn't reload the page
-    // ...
+    const input = document.getElementById('newItem') as HTMLInputElement
+    const newEntryText: string = input.value.trim()
+    if (!newEntryText) return // so we don't add empty items to our list
+    
   })
 
+  // handle the "Clear" button
   const clearItems = document.getElementById("clearItemsButton") as HTMLButtonElement
-  clearItems.addEventListener('click', )
+  clearItems.addEventListener('click', (): void => {
+    fullList.clearList() // clear the data stored in localStorage
+    template.clear() // clear what's displayed on the web page
+  })
+
+  fullList.load() // load the data
+  template.render(fullList) // load the corresponding HTML elements
 }
 
 // once the DOM content is loaded, run our initApp function
